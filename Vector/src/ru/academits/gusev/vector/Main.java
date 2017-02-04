f310a215f0f63946a4f51025367d9881c16595fb
AntonNSK <shir_nsk@mail.ru>
2017-02-05 01:43:17 +0600

    Создал Vector, выполнил все пункты.

---
+++ Vector/src/ru/academits/gusev/vector/Main.java
@@ -0,0 +1,30 @@
+package ru.academits.gusev.vector;
+
+import com.sun.org.apache.xpath.internal.SourceTree;
+
+public class Main {
+    public static void main(String[] args) {
+        double[] array = {1.2, 2.3, 1.7, 5.9, 6};
+        Vector vector1 = new Vector(3, array);
+        Vector vector2 = new Vector(7, array);
+        Vector vector3 = new Vector(vector2);
+        Vector vector4 = new Vector(6, array);
+        vector4.setComponent(1, 4);
+        vector4.setComponent(5, 3);
+
+
+        if (vector3.equals(vector2) && vector3.hashCode() == vector2.hashCode()) {
+            System.out.println("Вектор 2 равен вектору 3");
+        }
+
+        System.out.println("Сумма:" + vector2.sum(vector1).toString());
+        System.out.println("Разность:" + vector1.subtract(vector2).toString());
+        System.out.println("Умножение на скаляр (5):" + vector1.multiplyByScalar(5).toString());
+        System.out.println("Разворот:" + vector1.unwrap().toString());
+        System.out.printf("Длина: %.2f" + System.lineSeparator(), vector1.length());
+        System.out.println("Компонент под индексом 1:" + vector1.getComponent(1));
+        System.out.println("Сумма (static):" + Vector.sum(vector3, vector4).toString());
+        System.out.println("Разность (static):" + Vector.subtract(vector4, vector3).toString());
+        System.out.printf("Скалярное произведение:%.2f", Vector.calculateScalarProduct(vector4, vector3));
+    }
+}
---
+++ Vector/src/ru/academits/gusev/vector/Vector.java
@@ -0,0 +1,207 @@
+package ru.academits.gusev.vector;
+
+import java.util.Arrays;
+
+public class Vector {
+    private int n;
+    private double[] vector;
+
+    public Vector(int n) {
+        if (n < 0) {
+            throw new IllegalArgumentException("Не верное значение");
+        }
+        this.n = n;
+        vector = new double[n];
+    }
+
+    public Vector(Vector variant) {
+        this.n = variant.n;
+        this.vector = new double[n];
+        System.arraycopy(variant.vector, 0, this.vector, 0, variant.n);
+    }
+
+    Vector(double[] variant) {
+        n = variant.length;
+        vector = new double[n];
+        System.arraycopy(variant, 0, vector, 0, variant.length);
+    }
+
+    Vector(int n, double[] variant) {
+        if (n < 0) {
+            throw new IllegalArgumentException("Не верное значение");
+        }
+        this.n = n;
+        vector = new double[n];
+        if (n <= variant.length) {
+            System.arraycopy(variant, 0, vector, 0, n);
+        } else {
+            System.arraycopy(variant, 0, vector, 0, variant.length);
+        }
+    }
+
+    public int getSize() {
+        return n;
+    }
+
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("{ ");
+        for (int i = 0; i < n; ++i) {
+            sb.append(vector[i]);
+            if (i == n - 1) {
+                sb.append(" }");
+                break;
+            }
+            sb.append(", ");
+        }
+        return sb.toString();
+    }
+
+    public Vector sum(Vector variant) {
+        if (n >= variant.n) {
+            for (int i = 0; i < variant.n; ++i) {
+                vector[i] = vector[i] + variant.vector[i];
+            }
+            return this;
+        } else {
+            double[] vector1 = new double[variant.n];
+            for (int i = 0; i < variant.n; ++i) {
+                if (i < n) {
+                    vector1[i] = vector[i] + variant.vector[i];
+                } else {
+                    vector1[i] = variant.vector[i];
+                }
+            }
+            n = variant.n;
+            vector = vector1;
+            return this;
+        }
+    }
+
+    public Vector subtract(Vector variant) {
+        if (n >= variant.n) {
+            for (int i = 0; i < variant.n; ++i) {
+                vector[i] = vector[i] - variant.vector[i];
+            }
+            return this;
+        } else {
+            double[] vector1 = new double[variant.n];
+            for (int i = 0; i < variant.n; ++i) {
+                if (i < n) {
+                    vector1[i] = vector[i] - variant.vector[i];
+                } else {
+                    vector1[i] = vector1[i] - variant.vector[i];
+                }
+            }
+            n = variant.n;
+            vector = vector1;
+            return this;
+        }
+    }
+
+    public Vector multiplyByScalar(double a) {
+        for (int i = 0; i < n; ++i) {
+            vector[i] = vector[i] * a;
+        }
+        return this;
+    }
+
+    public Vector unwrap() {
+        for (int i = 0; i < n; ++i) {
+            if (vector[i] != 0) {
+                vector[i] = -vector[i];
+            }
+        }
+        return this;
+    }
+
+    public double length() {
+        double sum = 0;
+        for (int i = 0; i < n; ++i) {
+            sum += Math.pow(vector[i], 2);
+        }
+        return Math.sqrt(sum);
+    }
+
+    public double getComponent(int index) {
+        return vector[index];
+    }
+
+    public double setComponent(int index, double component) {
+        return vector[index] = component;
+    }
+
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null) {
+            return false;
+        }
+        Vector variant = (Vector) o;
+        for (int i = 0; i < n; ++i) {
+            if (vector[i] - variant.vector[i] >= 0.0001) {
+                return false;
+            }
+        }
+        return n == variant.n;
+    }
+
+    public int hashCode() {
+        int prime = 31;
+        int result = n;
+        for (int i = 0; i < n; ++i) {
+            result = prime * result + (int) vector[i];
+        }
+        return result;
+    }
+
+    public static Vector sum(Vector variant1, Vector variant2) {
+        if (variant1.n >= variant2.n) {
+            for (int i = 0; i < variant2.n; ++i) {
+                variant1.vector[i] = variant1.vector[i] + variant2.vector[i];
+            }
+            return new Vector(variant1.vector);
+        } else {
+            for (int i = 0; i < variant1.n; ++i) {
+                variant2.vector[i] = variant2.vector[i] + variant1.vector[i];
+            }
+            return new Vector(variant2.vector);
+        }
+    }
+
+    public static Vector subtract(Vector variant1, Vector variant2) {
+        if (variant1.n >= variant2.n) {
+            for (int i = 0; i < variant2.n; ++i) {
+                variant1.vector[i] = variant1.vector[i] - variant2.vector[i];
+            }
+            return new Vector(variant1.vector);
+        } else {
+            double[] vector1 = new double[variant2.n];
+            for (int i = 0; i < variant2.n; ++i) {
+                if (i < variant1.n) {
+                    vector1[i] = variant1.vector[i] - variant2.vector[i];
+                } else {
+                    vector1[i] = vector1[i] - variant2.vector[i];
+                }
+            }
+            variant1.vector = vector1;
+            return new Vector(variant1.vector);
+        }
+    }
+
+    public static double calculateScalarProduct(Vector variant1, Vector variant2) {
+        double scalarProduct = 0;
+        if (variant1.n >= variant2.n) {
+            for (int i = 0; i < variant2.n; ++i) {
+                scalarProduct = scalarProduct + variant1.vector[i] * variant2.vector[i];
+            }
+            return scalarProduct;
+        } else {
+            for (int i = 0; i < variant1.n; ++i) {
+                scalarProduct = scalarProduct + variant1.vector[i] * variant2.vector[i];
+            }
+            return scalarProduct;
+        }
+    }
+}
